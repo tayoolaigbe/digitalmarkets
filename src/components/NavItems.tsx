07086455720c153424454def6f373dbbcc684595
tayoolaigbe <tayoolaigbe@outlook.com>
2024-01-07 07:43:27 +0000

    Successfully added the Desktop Navbar

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { PRODUCTS_CATEGORIES } from '@/config';
+import { useEffect, useRef, useState } from 'react';
+import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
+
+const NavItems = () => {
+	const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+	const isAnyOpen = activeIndex !== null;
+
+	const navRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				setActiveIndex(null);
+			}
+		};
+
+		document.addEventListener('keydown', handler);
+		return () => {
+			document.removeEventListener('keydown', handler);
+		};
+	}, []);
+
+	useOnClickOutside(navRef, () => setActiveIndex(null));
+	return (
+		<div className="flex gap-4 h-full" ref={navRef}>
+			{PRODUCTS_CATEGORIES.map((category, index) => {
+				const handleOpen = () => {
+					if (activeIndex === index) {
+						setActiveIndex(null);
+					} else {
+						setActiveIndex(index);
+					}
+				};
+				const isOpen = index === activeIndex;
+				return (
+					<NavItem
+						category={category}
+						handleOpen={handleOpen}
+						isOpen={isOpen}
+						key={category.value}
+						isAnyOpen={isAnyOpen}
+					/>
+				);
+			})}
+		</div>
+	);
+};
+
+export default NavItems;
