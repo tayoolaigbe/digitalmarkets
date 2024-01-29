637513a0a038ac4eb52538248ad8ec1edcb4fb3f
Eyitayo <lohmanlaurie86@gmail.com>
2024-01-29 16:48:14 +0000

    Successfully configured TRPC

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Icons } from '@/components/Icons';
 import { Button, buttonVariants } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
@@ -5,8 +7,30 @@ import { Label } from '@/components/ui/label';
 import { cn } from '@/lib/utils';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	AuthCredentialsValidator,
+	TAuthCredentialsValidator,
+} from '@/lib/validators/account-crendentials-validators';
+import { trpc } from '@/trpc/client';
 
 const Page = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<TAuthCredentialsValidator>({
+		resolver: zodResolver(AuthCredentialsValidator),
+	});
+
+	const { data } = trpc.anyApiRoute.useQuery();
+	console.log(data);
+
+	const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+		// send data to server
+	};
+
 	return (
 		<>
 			<div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
@@ -27,13 +51,14 @@ const Page = () => {
 					</div>
 
 					<div className="grid gap-6">
-						<form>
+						<form onSubmit={handleSubmit(onSubmit)}>
 							<div className="grid gap-2">
 								<div className="grid gap-1 py-2">
 									<Label htmlFor="email">Emali</Label>
 									<Input
+										{...register('email')}
 										className={cn({
-											'focus-visible:ring-red-500': true,
+											'focus-visible:ring-red-500': errors.email,
 										})}
 										placeholder="you@example.com"
 									/>
@@ -41,8 +66,9 @@ const Page = () => {
 								<div className="grid gap-1 py-2">
 									<Label htmlFor="password">Passsword</Label>
 									<Input
+										{...register('password')}
 										className={cn({
-											'focus-visible:ring-red-500': true,
+											'focus-visible:ring-red-500': errors.password,
 										})}
 										placeholder="Password"
 										type="password"
